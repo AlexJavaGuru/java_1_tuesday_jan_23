08d6e1d06f7b453e14bf5b738a6712772c3493ae
evroleg <evroleg@gmail.com>
2023-01-17 21:42:26 +0200

    First Class

--- src/main/java/student_oleg_gryazev/_lesson_1/HelloWorld.java
+++ src/main/java/student_oleg_gryazev/_lesson_1/HelloWorld.java
@@ -1,4 +1,4 @@
-package student_oleg_gryazev;
+package student_oleg_gryazev._lesson_1;
 
 public class HelloWorld {
     public static void main(String[] args) {
---
+++ src/main/java/student_oleg_gryazev/_lesson_1/_level_4_5/Math.java
@@ -0,0 +1,12 @@
+package student_oleg_gryazev._lesson_1._level_4_5;
+
+public class Math {
+    public static void main(String[] args) {
+        int a = 74, b = 36, c = 50;
+
+        System.out.println(a+b);
+        System.out.println(c/3);
+
+
+    }
+}
