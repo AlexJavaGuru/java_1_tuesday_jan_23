b81f21736c244de85345045157329b0b780742b7
Оксана Tarasova <oksana_sineok@mail.ru>
2023-01-24 22:13:46 +0200

    test HelloWorld

--- src/main/java/student_Oksana_Tarasova/HelloWorld.java
+++ src/main/java/student_Oksana_Tarasova/HelloWorld.java
@@ -1,9 +1,8 @@
-package student_Oksana_Tarasova;
+package student_oksana_tarasova;
 
 public class HelloWorld {
 
     public static void main (String[] arg){
-
-        System. out. println("Hello World");
+        System.out.println("Hello World");
     }
 }
