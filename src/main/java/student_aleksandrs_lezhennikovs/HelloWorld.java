de794a67ba0da72d61e39e85c5c25dbc28fc2568
AlexandrLezh <sasha_l@inbox.lv>
2023-01-24 20:01:57 +0200

    Commit after join to Project

--- src/main/java/student_aleksandrs_lezhennikovs/HelloWorld.java
+++ src/main/java/student_aleksandrs_lezhennikovs/HelloWorld.java
@@ -3,6 +3,6 @@ package student_aleksandrs_lezhennikovs;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello World !!!");
+        System.out.println("Hello World !!! :)");
     }
 }
