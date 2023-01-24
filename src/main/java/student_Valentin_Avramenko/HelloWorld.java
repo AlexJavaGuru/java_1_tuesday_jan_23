dd46987c83102a5ceb590b93520057f0ee5f323a
ValAvr <trup2@inbox.lv>
2023-01-24 20:31:56 +0200

    Commit test 1

---
+++ src/main/java/student_Valentin_Avramenko/HelloWorld.java
@@ -0,0 +1,13 @@
+package student_Valentin_Avramenko;
+
+public class HelloWorld {
+
+    public static void main(String[] arg) {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] arg) {
+        System.out.println("Hello World test");
+    }
+
+}
