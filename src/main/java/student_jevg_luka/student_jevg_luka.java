17e37e12726a3875c331f4f09f8bfc0021c67089
jevgluka <zena68@inbox.lv>
2023-01-25 21:32:38 +0200

    student_jevg_luka

--- src/main/java/student_jevg_luka/student_jevg_luka.java
+++ src/main/java/student_jevg_luka/student_jevg_luka.java
@@ -2,7 +2,7 @@ package student_jevg_luka;
 
 public class student_jevg_luka {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] arg) {
+        System.out.println("Hello World!");
     }
 }
