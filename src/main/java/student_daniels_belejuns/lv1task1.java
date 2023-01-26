5678c603aac317127dbb89c7d0a233c756e505ac
d3n4ik-solo <danik-2002@inbox.lv>
2023-01-26 14:06:32 +0200

    LVL1TASK1

---
+++ src/main/java/student_daniels_belejuns/lv1task1.java
@@ -0,0 +1,21 @@
+package student_daniels_belejuns;
+
+import java.util.Scanner;
+
+public class lv1task1 {
+    public static void main (String... args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        int result = firstNumber * secondNumber;
+        System.out.println(firstNumber+secondNumber);
+        System.out.println(firstNumber-secondNumber);
+        System.out.println(firstNumber*secondNumber);
+        System.out.println(firstNumber/secondNumber);
+
+
+    }
+}
+
+
