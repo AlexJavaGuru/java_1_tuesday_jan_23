c2e580a8dddc2dc918e19b0b5f1bff7bb4b71284
AlexandrLezh <sasha_l@inbox.lv>
2023-01-26 10:14:06 +0200

    Added: lesson_2/level_senior done.

---
+++ src/main/java/student_aleksandrs_lezhennikovs/lesson_2/level_senior/SumTwoNumbers.java
@@ -0,0 +1,23 @@
+package student_aleksandrs_lezhennikovs.lesson_2.level_senior;
+
+// Исправьте ошибки компиляции в данной программе.
+
+        public class SumTwoNumbers {
+            public static void main(String[] args) {
+            int numberOne;
+            int numberTwo;
+
+            numberOne = 10;
+            numberTwo = 20;
+
+            System.out.println("Number 1 = " + numberOne);
+            System.out.println("Number 2 = " + numberTwo);
+
+
+
+            int sum = numberOne + numberTwo;
+
+            System.out.println("Sum = " + sum);
+        }
+
+    }
