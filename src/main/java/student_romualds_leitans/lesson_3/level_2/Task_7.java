265d75ef52f62098ab6c5bc79e9fc7b9a2859b21
Romualds Leitans <Romualdlei@gmail.com>
2023-02-01 20:16:34 +0000

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/student_romualds_leitans/lesson_3/level_2/Task_7.java
@@ -0,0 +1,8 @@
+package student_romualds_leitans.lesson_3.level_2;
+
+public class Task_7 {
+    public static void main(String[] args) {
+        String myName = "Romualds";
+        System.out.println("My name is "+ myName);
+    }
+}
