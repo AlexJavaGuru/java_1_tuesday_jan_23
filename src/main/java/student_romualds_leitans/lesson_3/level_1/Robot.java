38e4a3deed2944bb6e4e7d172ea3441a76af5217
Romualds Leitans <Romualdlei@gmail.com>
2023-02-01 19:58:16 +0000

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/student_romualds_leitans/lesson_3/level_1/Robot.java
@@ -0,0 +1,14 @@
+package student_romualds_leitans.lesson_3.level_1;
+
+public class Robot {
+    public void sayHello(){
+        System.out.println("Hello");}
+    public void sayYourName(){
+        System.out.println("My Name is ROBO");
+    }
+    public Robot(){
+
+    }
+}
+
+
---
+++ src/main/java/student_romualds_leitans/lesson_3/level_1/RobotDemo.java
@@ -0,0 +1,20 @@
+package student_romualds_leitans.lesson_3.level_1;
+
+class RobotDemo {
+    public static void main(String[] args) {
+        Robot robot = new Robot();
+        robot.sayHello();
+        robot.sayYourName();
+
+        Robot robot1 = new Robot();
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        Robot robot2 = new Robot();
+        robot2.sayHello();
+        robot2.sayYourName();
+
+
+    }
+
+}
