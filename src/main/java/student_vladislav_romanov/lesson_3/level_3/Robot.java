1d7631440a423f670e2ace8f5bc7064eb105a797
Vlad <wladislaw.romanov@gmail.com>
2023-02-02 20:33:53 +0300

    Lesson 3. Task 12.

--- src/main/java/student_vladislav_romanov/lesson_3/level_3/Robot.java
+++ src/main/java/student_vladislav_romanov/lesson_3/level_3/Robot.java
@@ -8,11 +8,15 @@ class Robot {
 
     }
 
+    public Robot(String robotName) {
+        this.name = robotName;
+    }
+
     public void sayHello() {
         System.out.println("Hello!");
     }
 
     public void sayYourName() {
-        System.out.println("My name is ROBO");
+        System.out.println("My name is " + this.name);
     }
 }
