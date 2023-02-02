4d428b587dae6624f155154ad16e1c4bfa079134
Vladislavs Romanovs <vladislavs.romanovs@swedbank.lv>
2023-02-02 11:17:05 +0200

    Lesson 3. Task 7.

---
+++ src/main/java/student_vladislav_romanov/lesson_3/level_2/Task_7.java
@@ -0,0 +1,10 @@
+package student_vladislav_romanov.lesson_3.level_2;
+
+public class Task_7 {
+
+    public static void main(String[] args) {
+        String myName = "Vladislav";
+
+        System.out.println(myName);
+    }
+}
