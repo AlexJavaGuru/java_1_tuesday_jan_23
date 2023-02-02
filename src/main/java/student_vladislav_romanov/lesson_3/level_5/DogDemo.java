17fafd8c68f2bebbee4e6884a425ce63785ee43b
Vlad <wladislaw.romanov@gmail.com>
2023-02-02 21:15:17 +0300

    Lesson 3. Task 20.

--- src/main/java/student_vladislav_romanov/lesson_3/level_5/DogDemo.java
+++ src/main/java/student_vladislav_romanov/lesson_3/level_5/DogDemo.java
@@ -5,6 +5,8 @@ public class DogDemo {
     public static void main(String[] args) {
         Dog dog = new Dog("Bobik", 3);
         dog.voice();
+        dog.happyBirthday();
+        dog.voice();
     }
 
 }
