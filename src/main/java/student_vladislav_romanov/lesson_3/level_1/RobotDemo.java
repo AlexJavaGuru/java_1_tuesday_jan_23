4a03fd3e35c33c01fc9b52f717145a9989b5bc3b
Vladislavs Romanovs <vladislavs.romanovs@swedbank.lv>
2023-02-02 11:15:16 +0200

    Lesson 3. Level 1. Task 6.

--- src/main/java/student_vladislav_romanov/lesson_3/level_1/RobotDemo.java
+++ src/main/java/student_vladislav_romanov/lesson_3/level_1/RobotDemo.java
@@ -3,8 +3,13 @@ package student_vladislav_romanov.lesson_3.level_1;
 public class RobotDemo {
 
     public static void main(String[] args) {
-        Robot robot = new Robot();
 
-        robot.sayHello();
+        Robot robot1 = new Robot();
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        Robot robot2 = new Robot();
+        robot2.sayHello();
+        robot2.sayYourName();
     }
 }
