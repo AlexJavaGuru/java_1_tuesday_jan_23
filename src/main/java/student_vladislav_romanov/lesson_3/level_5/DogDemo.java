49b1e78388d20ad1b9ec7e4da8fe398e7e6d5b54
Vlad <wladislaw.romanov@gmail.com>
2023-02-02 21:21:28 +0300

    Lesson 3. Task 22.

--- src/main/java/student_vladislav_romanov/lesson_3/level_5/DogDemo.java
+++ src/main/java/student_vladislav_romanov/lesson_3/level_5/DogDemo.java
@@ -5,6 +5,8 @@ public class DogDemo {
     public static void main(String[] args) {
         Dog dog = new Dog("Bobik", 3, "Rude");
         dog.voice();
+        dog.changeColor("White");
+        dog.voice();
     }
 
 }
