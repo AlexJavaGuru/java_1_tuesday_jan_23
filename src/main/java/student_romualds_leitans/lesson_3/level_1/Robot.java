065c8e293faedf06c5bfd3897c8b858f3294bfa6
Romualds Leitans <Romualdlei@gmail.com>
2023-02-02 09:47:57 +0000

    Merge remote-tracking branch 'origin/master'

--- src/main/java/student_romualds_leitans/lesson_3/level_1/Robot.java
+++ src/main/java/student_romualds_leitans/lesson_3/level_1/Robot.java
@@ -1,14 +1,16 @@
 package student_romualds_leitans.lesson_3.level_1;
 
 public class Robot {
-    public void sayHello(){
-        System.out.println("Hello");}
-    public void sayYourName(){
-        System.out.println("My Name is ROBO");
-    }
-    public Robot(){
+    String name;
 
+    public Robot(String RobotName){
+        this.name = RobotName;
+    }
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+    public void sayYourName() {
+        System.out.println("My Name is:" + this.name );
     }
 }
-
 
--- src/main/java/student_romualds_leitans/lesson_3/level_1/RobotDemo.java
+++ src/main/java/student_romualds_leitans/lesson_3/level_1/RobotDemo.java
@@ -2,19 +2,18 @@ package student_romualds_leitans.lesson_3.level_1;
 
 class RobotDemo {
     public static void main(String[] args) {
-        Robot robot = new Robot();
+        Robot robot = new Robot("Robocop");
         robot.sayHello();
         robot.sayYourName();
 
-        Robot robot1 = new Robot();
+        Robot robot1 = new Robot("Jahans");
         robot1.sayHello();
         robot1.sayYourName();
 
-        Robot robot2 = new Robot();
+        Robot robot2 = new Robot("Rider");
         robot2.sayHello();
         robot2.sayYourName();
-
-
+        }
     }
 
-}
+
