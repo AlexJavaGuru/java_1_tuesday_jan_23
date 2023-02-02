993ae4592eec0d4100ce266b38dc74074e98d55b
Vlad <wladislaw.romanov@gmail.com>
2023-02-02 20:25:49 +0300

    Lesson 3. Task 11.

---
+++ src/main/java/student_vladislav_romanov/lesson_3/level_3/Robot.java
@@ -0,0 +1,18 @@
+package student_vladislav_romanov.lesson_3.level_3;
+
+class Robot {
+
+    String name;
+
+    public Robot() {
+
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+}
