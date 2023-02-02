985b0587e87ad187c7f348478bbbe45761d896ed
Vlad <wladislaw.romanov@gmail.com>
2023-02-02 21:05:54 +0300

    Lesson 3. Task 19.

--- src/main/java/student_vladislav_romanov/lesson_3/level_5/Dog.java
+++ src/main/java/student_vladislav_romanov/lesson_3/level_5/Dog.java
@@ -3,15 +3,19 @@ package student_vladislav_romanov.lesson_3.level_5;
 class Dog {
 
     String name;
+    int age;
 
-    Dog(String dogName) {
-        this.name = dogName;
+    Dog(String dogsName) {
+        this.name = dogsName;
+    }
+
+    Dog(String dogsName, int dogsAge) {
+        this.name = dogsName;
+        this.age = dogsAge;
     }
 
     void voice() {
-        System.out.println(this.name);
-        System.out.println(this.name);
-        System.out.println(this.name);
+        System.out.println(this.name + " " + this.age);
     }
 
 }
--- src/main/java/student_vladislav_romanov/lesson_3/level_5/DogDemo.java
+++ src/main/java/student_vladislav_romanov/lesson_3/level_5/DogDemo.java
@@ -3,7 +3,7 @@ package student_vladislav_romanov.lesson_3.level_5;
 public class DogDemo {
 
     public static void main(String[] args) {
-        Dog dog = new Dog("Bobik");
+        Dog dog = new Dog("Bobik", 3);
         dog.voice();
     }
 
