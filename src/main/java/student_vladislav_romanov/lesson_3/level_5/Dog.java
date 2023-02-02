0fb3b7b524b97e2bf16ccb25aecc4d72f023eb36
Vlad <wladislaw.romanov@gmail.com>
2023-02-02 21:00:00 +0300

    Lesson 3. Task 18.

---
+++ src/main/java/student_vladislav_romanov/lesson_3/level_5/Dog.java
@@ -0,0 +1,17 @@
+package student_vladislav_romanov.lesson_3.level_5;
+
+class Dog {
+
+    String name;
+
+    Dog(String dogName) {
+        this.name = dogName;
+    }
+
+    void voice() {
+        System.out.println(this.name);
+        System.out.println(this.name);
+        System.out.println(this.name);
+    }
+
+}
---
+++ src/main/java/student_vladislav_romanov/lesson_3/level_5/DogDemo.java
@@ -0,0 +1,10 @@
+package student_vladislav_romanov.lesson_3.level_5;
+
+public class DogDemo {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("Bobik");
+        dog.voice();
+    }
+
+}
