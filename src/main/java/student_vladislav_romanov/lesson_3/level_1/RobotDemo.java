7794516e6744d20d4e012e5be0b43e28ee58cc02
Vladislavs Romanovs <vladislavs.romanovs@swedbank.lv>
2023-02-02 11:09:02 +0200

    Lesson 3. Level 1. Task 3, Task 4.

---
+++ src/main/java/student_vladislav_romanov/lesson_3/level_1/RobotDemo.java
@@ -0,0 +1,10 @@
+package student_vladislav_romanov.lesson_3.level_1;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+        Robot robot = new Robot();
+
+        robot.sayHello();
+    }
+}
