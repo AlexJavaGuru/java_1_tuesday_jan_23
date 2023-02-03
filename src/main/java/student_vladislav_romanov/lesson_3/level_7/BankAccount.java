e3f12679b08ab36149ded44283b5d4e96cb4b4e6
Vlad <wladislaw.romanov@gmail.com>
2023-02-03 21:53:17 +0300

    Lesson 3. Task 32

---
+++ src/main/java/student_vladislav_romanov/lesson_3/level_7/BankAccount.java
@@ -0,0 +1,21 @@
+package student_vladislav_romanov.lesson_3.level_7;
+
+class BankAccount {
+
+    String owner;
+    int money;
+
+    BankAccount(String owner, int moneyAmount) {
+        this.owner = owner;
+        this.money = moneyAmount;
+    }
+
+    String getOwner() {
+        return this.owner;
+    }
+
+    int getMoney() {
+        return this.money;
+    }
+
+}
