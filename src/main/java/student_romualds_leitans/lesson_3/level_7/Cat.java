cdb1b57168034707d2de013b0367714deda1bfd7
Romualds Leitans <Romualdlei@gmail.com>
2023-02-04 17:13:48 +0000

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/student_romualds_leitans/lesson_3/level_7/Cat.java
@@ -0,0 +1,27 @@
+package student_romualds_leitans.lesson_3.level_7;
+
+public class Cat {
+    String name;
+    int age;
+    String color;
+
+    public Cat(String newName, int newAge, String newColor){
+        this.name = newName;
+        this.age = newAge;
+        this.color = newColor;
+    }
+
+    void eat(){
+        System.out.println(" I eat fish ");
+    }
+
+    void sleep() {
+        System.out.println("zzzz Zzzz");
+
+    }
+    void play(){
+        System.out.println("Play with ball");
+
+    }
+
+}
