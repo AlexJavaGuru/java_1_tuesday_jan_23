02ee937b531987209c24edef2d31a13b7ff1f170
semitseva <semitsevanata@gmail.com>
2023-02-05 15:48:11 +0200

    NS_Lesson_3_ Robot_Task_13

--- src/main/java/student_natalja_semitseva/lesson_3_oop/Robot.java
+++ src/main/java/student_natalja_semitseva/lesson_3_oop/Robot.java
@@ -4,53 +4,67 @@ public class Robot {
 
     private String name;
     private int age;
-    private boolean isHungry;
 
-    public Robot(String robotName, int age, boolean isHungry){
+
+
+      public Robot(String robotName){
 
         this.name = robotName;
         this.age = age;
-        this.isHungry = isHungry;
 
     }
-    public void robotName(){
-        System.out.println("Peter");
+      public void Robot1() {
+            System.out.println("Rider");
 
 
+        }
+
+        public void robotName(){
+        System.out.println("Piter");
+
+    }
+
+    public void Robot2() {
+        System.out.println("John");
     }
 
     public void sayHello(){
         System.out.println("Hello!");
 
     }
-    public void sayYourName() {
-        System.out.println("My name is Peter");
+
+
+    public void Robot(String robotName) {
+
+    }
+
+    public void sayHello(String name) {
+
+        System.out.println("Hello");
 
 
 
     }
-    public void bark(){
 
-
-    }
-
-public void eat(){
-        isHungry = false;
-    System.out.println();
-
-}
-public void sleep(){
-    System.out.println();
-
-}
 
       public String getName() {
-        return name;
-
-
-
-      }
+          return name;
 
     }
+    public void sayYourName () {
+        System.out.println("My name is " + name);
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
 
 
