23cf4d4bf55b209a38b388a1d7e50bfd95e24d4e
d3n4ik-solo <danik-2002@inbox.lv>
2023-02-05 23:16:27 +0200

    DB: lesson_3 level_1

---
+++ src/main/java/student_daniels_belejuns/lesson_3/level_2/Task_8.java
@@ -0,0 +1,13 @@
+package student_daniels_belejuns.lesson_3.level_2;
+
+import java.util.Scanner;
+
+public class Task_8 {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is ur name?");
+        String name = input.next();
+        System.out.println("Hello" + name + "!");
+
+    }
+}
