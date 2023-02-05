13ecbdc40554a4eb5dfa5ed438079711d0b8aad4
d3n4ik-solo <danik-2002@inbox.lv>
2023-02-05 22:27:01 +0200

    DB: lesson_3 level_1

---
+++ src/main/java/student_daniels_belejuns/level_1/RobotDemo.java
@@ -0,0 +1,18 @@
+package student_daniels_belejuns.level_1;
+
+import student_daniels_belejuns.level_1.Robot;
+
+class RobotDemo {
+     public static void main(String[] args){
+
+         Robot bob = new Robot();
+         bob.sayHello();
+         bob.sayYOURname();
+
+         Robot bobs = new Robot();
+         bobs.sayHello();
+         bobs.sayYOURname();
+
+     }
+
+ }
