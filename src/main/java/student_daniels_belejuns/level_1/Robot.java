5a7dacdf24192b9084fd8b492460d506fff1207b
d3n4ik-solo <danik-2002@inbox.lv>
2023-02-05 22:27:01 +0200

    DB: lesson_3 level_1

---
+++ src/main/java/student_daniels_belejuns/level_1/Robot.java
@@ -0,0 +1,22 @@
+package student_daniels_belejuns.level_1;
+
+public class Robot{
+
+    public Robot(){
+
+    }
+public void sayHello(){
+System.out.println("Hello!");
+
+
+}
+public void sayYOURname(){
+        System.out.println("My name is ROBO");
+}
+
+
+
+
+}
+
+
