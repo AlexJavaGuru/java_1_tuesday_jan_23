0918a99d449bb919fc7b7a7f9128b13915bbd8aa
semitseva <semitsevanata@gmail.com>
2023-02-05 15:39:59 +0200

    NS_Lesson_3_ Robot_Task11_Task_12

--- src/main/java/student_natalja_semitseva/lesson_3_oop/Task_11.java
+++ src/main/java/student_natalja_semitseva/lesson_3_oop/Task_11.java
@@ -3,12 +3,13 @@ package student_natalja_semitseva.lesson_3_oop;
 public class Task_11 {
     public static void main(String[] args) {
 
-        Robot peter = new Robot("Peter",14, true); {
+        Robot peter = new Robot("Piter"); {
 
             String name = peter.getName();
 
            peter.sayHello();
-            peter.sayYourName();
+           peter.robotName();
+
 
 
 
