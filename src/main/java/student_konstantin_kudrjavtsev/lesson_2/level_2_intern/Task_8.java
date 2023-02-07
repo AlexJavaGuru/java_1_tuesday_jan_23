1676b4d624384395dcc388a2b0bc8d70bd78090b
qbands <kkudrjavtsev@gmail.com>
2023-02-07 23:44:20 +0200

    Konstantin Kudrjavtsev: Lesson 2: Homework

--- src/main/java/student_konstantin_kudrjavtsev/lesson_2/level_2_intern/Task_8.java
+++ src/main/java/student_konstantin_kudrjavtsev/lesson_2/level_2_intern/Task_8.java
@@ -1,11 +1,11 @@
-package student_konstantin_kudrjavtsev.lesson_1;
+package student_konstantin_kudrjavtsev.lesson_2.level_2_intern;
 
 import java.util.Scanner;
 
 public class Task_8 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Name Surname");
+        System.out.println("Name Surename");
         String name = input.next();
         System.out.println("Hello Konstantin Kudrjavtsev");
     }
---
+++ src/main/java/student_konstantin_kudrjavtsev/lesson_2/level_5_middle/Task_15.java
@@ -0,0 +1,7 @@
+package student_konstantin_kudrjavtsev.lesson_2.level_5_middle;
+
+public class Task_15 {
+    public static void main(String[] args) {
+        System.out.print("text");
+    }
+}
