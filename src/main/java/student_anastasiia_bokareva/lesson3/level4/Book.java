c049f2a8b05cf7d605ce0a6f3b2a7ce31f3e2bfe
IchiuRain <ichiu.rain011@gmail.com>
2023-02-07 14:09:30 +0200

    hi

---
+++ src/main/java/student_anastasiia_bokareva/lesson3/level4/Book.java
@@ -0,0 +1,13 @@
+package student_anastasiia_bokareva.lesson3.level4;
+
+public class Book {
+    String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return title;
+    }
+}
