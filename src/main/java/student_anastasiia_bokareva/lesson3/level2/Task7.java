bd7e462f8badc7931a989b82778dbcc78eb5f0cd
IchiuRain <ichiu.rain011@gmail.com>
2023-02-07 10:21:55 +0200

    hi

---
+++ src/main/java/student_anastasiia_bokareva/lesson3/level2/Task7.java
@@ -0,0 +1,8 @@
+package student_anastasiia_bokareva.lesson3.level2;
+
+public class Task7 {
+    public static void main(String[] args) {
+        String myName ="Nastya";
+        System.out.println(myName);
+    }
+}
