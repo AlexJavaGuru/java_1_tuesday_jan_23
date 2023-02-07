4bb8598ecfa8b313f24ee501f18f682dc123750e
CarlBraun <yaroslav689@gmail.com>
2023-02-07 23:13:58 +0200

    Added: Lession 1 Homework finished, Lession 2 Homework Level 1, Task 1.

--- src/main/java/student_yaraslau_bardzinovich/lesson_2/homework/level_1_intern/Task_1.java
+++ src/main/java/student_yaraslau_bardzinovich/lesson_2/homework/level_1_intern/Task_1.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 public class Task_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Enter first number:");
         int firstNumber = scanner.nextInt();
+        System.out.println("Enter second number:");
         int secondNumber = scanner.nextInt();
         int Multiply = firstNumber * secondNumber;
         int Divide = firstNumber / secondNumber;
         int Plus = firstNumber + secondNumber;
         int Minus = firstNumber - secondNumber;
 
-        System.out.println(Multiply);
-        System.out.println(Divide);
-        System.out.println(Plus);
-        System.out.println(Minus);
+        System.out.println("Multiply:" + Multiply);
+        System.out.println("Divide:" + Divide);
+        System.out.println("Plus:" + Plus);
+        System.out.println("Minus:" + Minus);
     }
 
 }
