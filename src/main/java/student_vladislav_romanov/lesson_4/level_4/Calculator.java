888aacb837b160a7d015b8eaf3e9fd6e34c75425
Vlad <wladislaw.romanov@gmail.com>
2023-02-08 22:06:54 +0300

    Lesson 4. Task 11

---
+++ src/main/java/student_vladislav_romanov/lesson_4/level_4/Calculator.java
@@ -0,0 +1,21 @@
+package student_vladislav_romanov.lesson_4.level_4;
+
+class Calculator {
+
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int sub(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int mul(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public int div(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+}
