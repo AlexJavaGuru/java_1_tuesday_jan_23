60cda752deb9a4fbeb2e03a2bd0707db00074e14
Vlad <wladislaw.romanov@gmail.com>
2023-02-08 21:30:14 +0300

    Lesson 4. Task 5

---
+++ src/main/java/student_vladislav_romanov/lesson_4/level_2/Task_5.java
@@ -0,0 +1,21 @@
+package student_vladislav_romanov.lesson_4.level_2;
+
+import java.util.Scanner;
+
+class Task_5 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите два целых числа: ");
+
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+
+        if (first > second) {
+            System.out.println(first);
+        } else {
+            System.out.println(second);
+        }
+    }
+
+}
