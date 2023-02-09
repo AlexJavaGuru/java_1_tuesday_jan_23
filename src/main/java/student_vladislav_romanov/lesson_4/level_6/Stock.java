b7c1ecd6f37eea824fa49715ac84ce532174898e
Vlad <wladislaw.romanov@gmail.com>
2023-02-09 22:07:36 +0300

    Lesson 4. Task 17

---
+++ src/main/java/student_vladislav_romanov/lesson_4/level_6/Stock.java
@@ -0,0 +1,31 @@
+package student_vladislav_romanov.lesson_4.level_6;
+
+class Stock {
+
+    String company;
+    int currentPrice;
+    int minPrice;
+    int maxPrice;
+
+    Stock (String name, int price) {
+        company = name;
+        currentPrice = price;
+        minPrice = price;
+        maxPrice = price;
+    }
+
+    void updatePrice(int newPrice) {
+        currentPrice = newPrice;
+        if (newPrice < minPrice) {
+            minPrice = newPrice;
+        }
+        if (newPrice > maxPrice) {
+            maxPrice = newPrice;
+        }
+    }
+
+    String getPriceInformation() {
+        return "Company = " + company + ", Current Price = " + currentPrice + ", Min Price = " + minPrice + ", Max Price = " + maxPrice;
+    }
+
+}
