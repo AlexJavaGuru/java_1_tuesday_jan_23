457283b461ff5c9c79c9e64ffeef52855520d03b
semitseva <semitsevanata@gmail.com>
2023-02-12 16:59:49 +0200

    NS_Lesson_3

---
+++ src/main/java/student_natalja_semitseva/lesson_3_oop/BookDemo.java
@@ -0,0 +1,13 @@
+package student_natalja_semitseva.lesson_3_oop;
+
+import student_natalja_semitseva.lesson_2.level_2.Book;
+
+public class BookDemo {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
