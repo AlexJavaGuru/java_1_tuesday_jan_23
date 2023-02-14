4cc1d528d16075d7907a6b2a9aa738cdc44cfb3f
Evroleg <evroleg@gmail.com>
2023-02-14 21:12:36 +0200

    Some changes

---
+++ src/main/java/student_oleg_gryazev/_lesson_3/level_7/cirkleDemo.java
@@ -0,0 +1,9 @@
+package student_oleg_gryazev._lesson_3.level_7;
+
+public class cirkleDemo {
+    public static void main(String[] args) {
+        Circle cirkle = new Circle();
+        cirkle.calculateArea(5);
+
+    }
+}
