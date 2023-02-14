5a88e08d5dcea2e50f22f63fee4824d0c2e014cb
Evroleg <evroleg@gmail.com>
2023-02-14 20:12:18 +0200

    Some changes

---
+++ src/main/java/student_oleg_gryazev/_lesson_3/DemoTrafficLightControlsTheCar.java
@@ -0,0 +1,12 @@
+package student_oleg_gryazev._lesson_3;
+
+public class DemoTrafficLightControlsTheCar {
+    public static void main(String[] args) {
+        TrafficLightControlsTheCar car = new TrafficLightControlsTheCar();
+        TrafficLightControlsTheCar trafficLight = new TrafficLightControlsTheCar();
+
+        car.carPosition();
+        trafficLight.positionOfTrafficLight();
+    }
+
+}
