85b7a3a93095c1da9f10858d9f0a24337291321d
AlexandrLezh <sasha_l@inbox.lv>
2023-02-15 22:21:03 +0200

    lesson_5/level_3; level_4 tunning

--- src/main/java/student_aleksandrs_lezhennikovs/lesson_5/level_4/Task_27.java
+++ src/main/java/student_aleksandrs_lezhennikovs/lesson_5/level_4/Task_27.java
@@ -8,10 +8,28 @@ public class Task_27 {
     public static void main(String[] args) {
 
         int[] numbers  = createRandomSizeArray();
+        fillArrayWithRandomElements(numbers);
         System.out.println("Your array: " + Arrays.toString(numbers));
 
         int maxResult = maxElementsInArray(numbers);
         System.out.println("Max element in array is: " + maxResult);
+    }
+
+    private static int[] createRandomSizeArray() {
+
+        Random random = new Random();
+        int arraySize = random.nextInt(10) + 1;
+        int[] numbers = new int[arraySize];
+
+        return numbers;
+    }
+
+    private static void fillArrayWithRandomElements(int[] numbers) {
+
+        Random random = new Random();
+        for (int count = 0; count < numbers.length; count++) {
+            numbers[count] = random.nextInt(100);
+        }
     }
 
     private static int maxElementsInArray(int[] numbers) {
@@ -19,28 +37,12 @@ public class Task_27 {
         int result = numbers[0];
         for (int number : numbers) {
             result = maxOfTwo(result, number);
-            }
+        }
         return result;
     }
 
     private static int maxOfTwo(int firstNumber, int secondNumber) {
 
         return (firstNumber >= secondNumber) ? firstNumber : secondNumber;
-    }
-
-    private static int[] createRandomSizeArray() {
-
-        Random random = new Random();
-        int[] numbers = new int[sizeOfArray()];
-        for (int count = 0; count < numbers.length; count++) {
-            numbers[count] = random.nextInt(100);
-        }
-        return numbers;
-    }
-
-    private static int sizeOfArray() {
-
-        Random random = new Random();
-        return random.nextInt(10) + 1;
     }
 }
