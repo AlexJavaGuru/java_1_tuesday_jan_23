059738ffc13edfb16cecf76661921cf1ea598a8b
Оксана Tarasova <oksana_sineok@mail.ru>
2023-02-16 12:51:41 +0200

    commit OksTar_lesson 5/1

---
+++ src/main/java/student_oksana_tarasova/lesson_5/level_2/Array2.java
@@ -0,0 +1,20 @@
+package student_oksana_tarasova.lesson_5.level_2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array2 {
+    public static void main(String[] args) {
+        System.out.println("Input three whole numbers...");
+        Scanner number = new Scanner(System.in);
+
+        int[] array = new int[3];
+        array[0] = number.nextInt();
+        array[1] = number.nextInt();
+        array[2] = number.nextInt();
+
+        System.out.println("Array cell 0 = " + array[0]);
+        System.out.println("Array cell 1 = " + array[1]);
+        System.out.println("Array cell 3 = " + array[2]);
+    }
+}
