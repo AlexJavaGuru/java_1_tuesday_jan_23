73c1288fa62d9ad403e98921f5d7ef89c2069d6e
Vlad <wladislaw.romanov@gmail.com>
2023-02-16 23:00:04 +0300

    Lesson 5. Task 32

--- src/main/java/student_vladislav_romanov/lesson_5/level_5/ArrayUtil.java
+++ src/main/java/student_vladislav_romanov/lesson_5/level_5/ArrayUtil.java
@@ -1,9 +1,19 @@
 package student_vladislav_romanov.lesson_5.level_5;
 
+import java.util.Random;
+
 class ArrayUtil {
+
+    Random random = new Random();
 
     public int[] createArray(int arrayLength) {
         return new int[arrayLength];
+    }
+
+    public void fillArrayWithRandomNumbers(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
     }
 
 }
