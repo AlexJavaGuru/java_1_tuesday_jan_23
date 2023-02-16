d36a079e019589cdce97d9e3eac5dccff917ce6c
Vlad <wladislaw.romanov@gmail.com>
2023-02-16 22:32:59 +0300

    Lesson 5. Task 29

---
+++ src/main/java/student_vladislav_romanov/lesson_5/level_4/Task_29.java
@@ -0,0 +1,34 @@
+package student_vladislav_romanov.lesson_5.level_4;
+
+import java.util.Random;
+
+class Task_29 {
+
+    public static void main(String[] args) {
+        int[] numbers;
+
+        int arrayLength;
+        Random random = new Random();
+
+        arrayLength = random.nextInt(10);
+        numbers = new int[arrayLength];
+
+        for (int i = 0; i < arrayLength; i++) {
+            numbers[i] = random.nextInt(100);
+        }
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+
+        System.out.println("Even numbers in array:");
+
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+        }
+
+    }
+
+}
