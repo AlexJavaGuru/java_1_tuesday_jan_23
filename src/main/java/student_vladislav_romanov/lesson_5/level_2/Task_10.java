4478dea5eb2ab6fa771497f8eff8d71e4f4918a0
Vlad <wladislaw.romanov@gmail.com>
2023-02-16 21:07:41 +0300

    Lesson 5. Task 10

---
+++ src/main/java/student_vladislav_romanov/lesson_5/level_2/Task_10.java
@@ -0,0 +1,12 @@
+package student_vladislav_romanov.lesson_5.level_2;
+
+class Task_10 {
+
+    public static void main(String[] args) {
+        int[] numbers = {3,7,1};
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+}
