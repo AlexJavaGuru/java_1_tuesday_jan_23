f00de5c92ab834ff824adc5d552d02ba82d75907
Vlad <wladislaw.romanov@gmail.com>
2023-02-16 22:28:25 +0300

    Lesson 5. Task 28

---
+++ src/main/java/student_vladislav_romanov/lesson_5/level_4/Task_28.java
@@ -0,0 +1,30 @@
+package student_vladislav_romanov.lesson_5.level_4;
+
+import java.util.Random;
+
+class Task_28 {
+
+    public static void main(String[] args) {
+        int[] numbers;
+        int arrayLength;
+        int minNumber = 100;
+        Random random = new Random();
+
+        arrayLength = random.nextInt(10);
+        numbers = new int[arrayLength];
+
+        for (int i = 0; i < arrayLength; i++) {
+            numbers[i] = random.nextInt(minNumber);
+        }
+
+        for (int number : numbers) {
+            System.out.println(number);
+            if (number < minNumber) {
+                minNumber = number;
+            }
+        }
+
+        System.out.println("Min number in array is " + minNumber);
+    }
+
+}
