f450cdd83584fa1f335d8d8327ee2f0b8c3b15c6
NatalijaNovel <natalijariga@inbox.lv>
2023-02-17 21:07:20 +0200

    NR: Lesson_4 Homework

--- src/main/java/student_natalija_rasponomarjova/lesson_4/level_7/LeapYear.java
+++ src/main/java/student_natalija_rasponomarjova/lesson_4/level_7/LeapYear.java
@@ -10,15 +10,34 @@ package student_natalija_rasponomarjova.lesson_4.level_7;
         // Если не делится, значит это не столетие и можно сделать вывод, что год високосный.
         // Если делится на 100, значит это столетие и его следует проверить его делимость на 400.
         // Если год делится на 400, то он високосный.
-        // Иначе год обычный.
-    }}*/
+        // Иначе год обычный.}}*/
 class LeapYear {
-
     public boolean isLeapYear(int year) {
-
-        if (year % 4 != 0) {
+        if (!isDividedBy4(year)) {
+            return false;
         }
-        return false;
+
+        if (!isDividedBy100(year)) {
+            return true;
+        }
+
+        return isDividedBy400(year);
     }
 
+    private boolean isDividedBy4(int year) {
+
+        return year % 4 == 0;
+    }
+
+    private boolean isDividedBy100(int year) {
+
+        return year % 100 == 0;
+    }
+
+    private boolean isDividedBy400(int year) {
+
+        return year % 400 == 0;
+    }
 }
+
+
