fb0183c4e665cc678e3a9710bb879fcca8ad0b19
AndrejsCekalins <batarejka2006@gmail.com>
2023-02-18 16:00:09 +0200

    AC_lesson_5

---
+++ src/main/java/student_andrejs_cekalins/lesson_5/level_5/Task_32/ArrayUtilTest.java
@@ -0,0 +1,14 @@
+package student_andrejs_cekalins.lesson_5.level_5.Task_32;
+
+public class ArrayUtilTest {
+    public static void main(String[] args) {
+        ArrayUtilTest test = new ArrayUtilTest();
+        test.shouldCreateArray();
+    }
+
+    public void shouldCreateArray() {
+
+
+    }
+
+}
