a6db5091f694a30611641d8e30b956da0d444d94
Vlad <wladislaw.romanov@gmail.com>
2023-02-21 22:15:19 +0300

    Lesson 6. Task 4

---
+++ src/main/java/student_vladislav_romanov/lesson_6/level_2/WhileDefinition4.java
@@ -0,0 +1,13 @@
+package student_vladislav_romanov.lesson_6.level_2;
+
+class WhileDefinition4 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+}
