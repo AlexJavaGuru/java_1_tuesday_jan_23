21f3815d9624d5ae0f7c200800f593652d879e6f
Vlad <wladislaw.romanov@gmail.com>
2023-02-22 17:55:39 +0300

    Lesson 6. Task 8

--- src/main/java/student_vladislav_romanov/lesson_6/level_2/DayOfTheWeekDetector.java
+++ src/main/java/student_vladislav_romanov/lesson_6/level_2/DayOfTheWeekDetector.java
@@ -10,4 +10,17 @@ class DayOfTheWeekDetector {
         return scanner.nextInt();
     }
 
+    public String findDayOfTheWeek(int dayNumber) {
+        return switch (dayNumber) {
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            case 7 -> "Sunday";
+            default -> "Not correct day number";
+        };
+    }
+
 }
