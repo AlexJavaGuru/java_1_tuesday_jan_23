acd8bfaf76e89ac23f9e68261516e2a6aa31479f
Оксана Tarasova <oksana_sineok@mail.ru>
2023-02-23 17:36:48 +0200

    commit OksTar_lesson 6/1

--- src/main/java/student_oksana_tarasova/lesson_6/level_3/ArrayService.java
+++ src/main/java/student_oksana_tarasova/lesson_6/level_3/ArrayService.java
@@ -1,21 +1,21 @@
 package student_oksana_tarasova.lesson_6.level_3;
 
-/*class ArrayService {
-  /*  int[] array = new int[];
+class ArrayService {
 
-
-    ArrayService(int[] array) {
-        this.array = array;
-
-
-    }
+    int givenNumber;
 
     public boolean containsTheGivenNumberInTheArray(int[] array, int givenNumber) {
         for (int numberArray : array) {
             if (givenNumber == numberArray) {
-            } continue;
+            }
         }
-        return true;
+    }
+
+    public void checkResult(boolean state, ) {
+        if(state) {
+            System.out.println("The array contains the number " + givenNumber);
+        }
+        System.out.println("The array does not contains the number " + givenNumber);
     }
 }
-*/
+
--- src/main/java/student_oksana_tarasova/lesson_6/level_3/ArrayServiceDemo.java
+++ src/main/java/student_oksana_tarasova/lesson_6/level_3/ArrayServiceDemo.java
@@ -5,13 +5,14 @@ import java.util.Arrays;
 class ArrayServiceDemo {
 
     public static void main(String[] args) {
-
+        ArrayService arrayService = new ArrayService();
         int[] array = {3, 5, 7, 1, 3, 9, 11, 13};
-        System.out.println(Arrays.toString(array));
-        int givenNumber = 2;
+        arrayService.containsTheGivenNumberInTheArray(array, 5);
 
 
-        for (int numberArray : array) {
+
+
+    /*   for (int numberArray : array) {
             if (givenNumber == numberArray) {
                 System.out.println("yes");
 
@@ -19,6 +20,7 @@ class ArrayServiceDemo {
         }
 
     }
-
+*/
+    }
 }
 
