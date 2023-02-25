2ea4a89d7d0b9f7f477f9a04829f1f81779d2125
AndrejsCekalins <batarejka2006@gmail.com>
2023-02-25 08:37:28 +0200

    AC_lesson_6

---
+++ src/main/java/student_andrejs_cekalins/lesson_6/level_1/Task_6/NumberUtils.java
@@ -0,0 +1,7 @@
+package student_andrejs_cekalins.lesson_6.level_1.Task_6;
+
+class NumberUtils {
+    public boolean isEven(int number) {
+        return (number % 2 == 0);
+    }
+}
---
+++ src/main/java/student_andrejs_cekalins/lesson_6/level_3/ArrayService.java
@@ -0,0 +1,4 @@
+package student_andrejs_cekalins.lesson_6.level_3;
+
+public class ArrayService {
+}
