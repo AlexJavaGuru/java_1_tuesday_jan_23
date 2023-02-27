0eb7eafe992b43c49edafb6db09f73c1c343a24b
JulijaJulija <julisa1204@gmail.com>
2023-02-27 21:11:31 +0200

    Lesson 6

--- src/main/java/student_julija_raudive/lesson_6/level_2/DayOfTheWeekDetector.java
+++ src/main/java/student_julija_raudive/lesson_6/level_2/DayOfTheWeekDetector.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 class DayOfTheWeekDetector {
 
 
-
-
     public int getDayNumberFromUser() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input day number between 1 and 7 : ");
@@ -17,37 +15,15 @@ class DayOfTheWeekDetector {
     public String findDayOfTheWeek(int dayNumber) {
         String dayOfTheWeek;
         switch (dayNumber) {
-            case 1 -> {
-                dayOfTheWeek = "Monday";
-                break;
-            }
-            case 2 -> {
-                dayOfTheWeek = "Tuesday";
-                break;
-            }
-            case 3 -> {
-                dayOfTheWeek = "Wednesday";
-                break;
-            }
-            case 4 -> {
-                dayOfTheWeek = "Thursday";
-                break;
-            }
-            case 5 -> {
-                dayOfTheWeek = "Friday";
-                break;
-            }
-            case 6 -> {
-                dayOfTheWeek = "Saturday";
-                break;
-            }
-            case 7 -> {
-                dayOfTheWeek = "Sunday";
-                break;
-            }
-            default -> {
-                dayOfTheWeek = "Not correct day number";
-            }
+            case 1 -> dayOfTheWeek = "Monday";
+            case 2 -> dayOfTheWeek = "Tuesday";
+            case 3 -> dayOfTheWeek = "Wednesday";
+            case 4 -> dayOfTheWeek = "Thursday";
+            case 5 -> dayOfTheWeek = "Friday";
+            case 6 -> dayOfTheWeek = "Saturday";
+            case 7 -> dayOfTheWeek = "Sunday";
+            default -> dayOfTheWeek = "Not correct day number";
+
         }
         return dayOfTheWeek;
     }
