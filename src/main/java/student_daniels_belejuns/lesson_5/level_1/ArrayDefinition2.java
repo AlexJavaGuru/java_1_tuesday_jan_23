2f30ff5b637947215e0ebe40e05d990f743114cc
d3n4ik-solo <danik-2002@inbox.lv>
2023-02-28 21:34:40 +0200

    DB:LESSON_5

---
+++ src/main/java/student_daniels_belejuns/lesson_5/level_1/ArrayDefinition2.java
@@ -0,0 +1,9 @@
+package student_daniels_belejuns.lesson_5.level_1;
+
+class ArrayDefinition2 {
+
+    public static void main(String[] args) {
+        byte[] numbers = new byte[10];
+    }
+
+}
---
+++ src/main/java/student_daniels_belejuns/lesson_5/level_2/task_10.java
@@ -0,0 +1,11 @@
+package student_daniels_belejuns.lesson_5.level_2;
+
+import java.util.Arrays;
+
+public class task_10 {
+public static void main(String[]args){
+    int[] array = {1, 3, 7};
+    System.out.println(Arrays.toString(array));
+
+}
+}
