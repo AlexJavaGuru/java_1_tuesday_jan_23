4c08d02b720618b2c5866eb562a72ec4913324a6
Evroleg <evroleg@gmail.com>
2023-03-02 21:47:45 +0200

    Lesson 4 level 1-10 Changes

--- src/main/java/student_oleg_gryazev/lesson_4/level_5/LightColorDetector.java
+++ src/main/java/student_oleg_gryazev/lesson_4/level_5/LightColorDetector.java
@@ -2,55 +2,23 @@ package student_oleg_gryazev.lesson_4.level_5;
 
 public class LightColorDetector {
 
-    int wavelength;
-
     public String detect(int wavelength) {
         if (wavelength >= 380 && wavelength <= 449) {
-            System.out.println("Violet");
             return "Violet";
         } else if (wavelength >= 450 && wavelength <= 494) {
-            System.out.println("Blue");
             return "Blue";
         } else if (wavelength >= 495 && wavelength <= 569) {
-            System.out.println("Green");
             return "Green";
         } else if (wavelength >= 570 && wavelength <= 589) {
-            System.out.println("Yellow");
             return "Yellow";
         } else if (wavelength >= 590 && wavelength <= 619) {
-            System.out.println("Orange");
             return "Orange";
         }
         if (wavelength >= 620 && wavelength <= 750) {
-            System.out.println("Red");
             return "Red";
         } else
             return "Invisible";
     }
 
-    void testVisibleSpectrum() {
-        for (int i = 380; i < 751; i++) {
-            detect(i);
-            i++;
-        }
-    }
-
-    void testInvisibleSpectrumBelow() {
-        for (int i = 0; i < 380; i++) {
-            detect(i);
-            if (i > 0) {
-                System.out.println("Spectrum from 0 to 380 wavelenght is invisible");
-            }
-        }
-    }
-
-    void testInvisibleSpectrum() {
-        for (int j = 750; j < 1000; j++) {
-            detect(j);
-            if (j > 750) {
-                System.out.println("Spectrum from 751 to 1000 wavelenght is invisible");
-            }
-        }
-    }
 }
 
--- src/main/java/student_oleg_gryazev/lesson_4/level_7_task24/Calculator.java
+++ src/main/java/student_oleg_gryazev/lesson_4/level_7_task24/Calculator.java
@@ -1,6 +1,7 @@
 package student_oleg_gryazev.lesson_4.level_7_task24;
 
-class Calculator {
+public class Calculator {
+
     public int sum(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
     }
@@ -20,60 +21,10 @@ class Calculator {
     public boolean isEven(int number) {
         return number % 2 == 0;
     }
-}
-
-
-class CalculatorTest {
-    public static void main(String[] args) {
-        int result;
-        boolean bulResult;
-        Calculator calculator = new Calculator();
-
-        result = calculator.sum(35, 40);
-        if (result == 75) {
-            System.out.println("Sum test = OK");
-        } else {
-            System.out.println("Sum test = FAIL");
-        }
-
-        result = calculator.sub(35, 15);
-
-        if (result == 20) {
-            System.out.println("Sub test = OK");
-        } else {
-            System.out.println("Sub test = FAIL");
-        }
-
-
-        result = calculator.mul(5, 5);
-
-        if (result == 25) {
-            System.out.println("Mul test = OK");
-        } else {
-            System.out.println("Mul test = FAIL");
-        }
-
-        result = calculator.div(40, 8);
-        if (result == 5) {
-            System.out.println("Div test = OK");
-        } else {
-            System.out.println("Div test = FAIL");
-        }
-
-
-        bulResult = calculator.isEven(40);
-        if (bulResult) {
-            System.out.println("isEven test = OK");
-        } else {
-            System.out.println("isEven test = FAIL");
-        }
-
-
-        bulResult = calculator.isEven(41);
-        if (!bulResult) {
-            System.out.println("isEven test = OK");
-        } else {
-            System.out.println("isEven test = FAIL");
-        }
+    public boolean isOdd(int number) {
+        return number % 2 != 0;
     }
+
+
 }
+
