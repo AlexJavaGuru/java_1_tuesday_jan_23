e10ef7bf6ef88f790e29014cea5c81080928487d
Evroleg <evroleg@gmail.com>
2023-03-06 10:15:03 +0200

    Lesson 5 level 5 Changes

---
+++ src/main/java/student_oleg_gryazev/lesson_6/_level_1/task_6/NumberUtil.java
@@ -0,0 +1,9 @@
+package student_oleg_gryazev.lesson_6._level_1.task_6;
+
+public class NumberUtil {
+    public boolean isEven(int number)
+    {
+        return number % 2 == 0;
+    }
+
+}
---
+++ src/main/java/student_oleg_gryazev/lesson_6/_level_1/task_6/NumberUtilTest.java
@@ -0,0 +1,39 @@
+package student_oleg_gryazev.lesson_6._level_1.task_6;
+
+public class NumberUtilTest {
+
+    public static void main(String[] args) {
+        NumberUtilTest test = new NumberUtilTest();
+        test.isEvenTrueTest();
+        test.isEvenFalse();
+    }
+
+    void isEvenTrueTest() {
+        NumberUtil numberUtil = new NumberUtil();
+
+        boolean expectedResultEven = true;
+        boolean actualResult = numberUtil.isEven(10);
+
+        if (expectedResultEven == actualResult) {
+            System.out.println("Test is passed, the number is even.");
+        } else {
+            System.out.println("Test is failed!");
+            System.out.println("Expected result " + expectedResultEven + "actual result " + actualResult);
+        }
+    }
+
+    void isEvenFalse() {
+        NumberUtil numberUtil = new NumberUtil();
+
+        boolean expectedResult = false;
+        boolean actualResult = numberUtil.isEven(11);
+
+        if (expectedResult == actualResult) {
+            System.out.println("Test is passed, the number is odd.");
+        } else {
+            System.out.println("Test is failed!");
+        }
+    }
+
+}
+
