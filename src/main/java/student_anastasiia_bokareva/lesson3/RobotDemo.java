d5422661f4f989c7bed9471aaacb110dc7eb10d6
Evroleg <evroleg@gmail.com>
2023-03-06 10:28:17 +0200

    Revert "hi"
    This reverts commit 171f56e6cddad98730b06ce3cd2f8c09cf276031.

--- src/main/java/student_anastasiia_bokareva/lesson3/RobotDemo.java
+++
@@ -1,4 +0,0 @@
-package student_anastasiia_bokareva.lesson3;
-
-public class RobotDemo {
-}
--- src/main/java/student_anastasiia_bokareva/lesson3/SayHello.java
+++
@@ -1,5 +0,0 @@
-package student_anastasiia_bokareva.lesson3;
-
-public class SayHello {
-    System.out.println("Hello");
-}
