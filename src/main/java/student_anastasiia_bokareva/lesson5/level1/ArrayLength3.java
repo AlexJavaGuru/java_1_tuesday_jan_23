d7974b04fcd4aca782d74e46cf90120ae4f8bcf2
IchiuRain <ichiu.rain011@gmail.com>
2023-03-07 19:26:07 +0200

    ABok_lesson3_DogUpdate

---
+++ src/main/java/student_anastasiia_bokareva/lesson5/level1/ArrayLength3.java
@@ -0,0 +1,8 @@
+package student_anastasiia_bokareva.lesson5.level1;
+
+public class ArrayLength3 {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers.length());
+    }
+}
