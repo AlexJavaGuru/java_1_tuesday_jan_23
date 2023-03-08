b614e80b9c161797706e08ae962969aa0260645f
Vlad <wladislaw.romanov@gmail.com>
2023-03-08 22:12:56 +0300

    Lesson 7. Task 15

---
+++ src/main/java/student_vladislav_romanov/lesson_7/level_7/UserEntity.java
@@ -0,0 +1,92 @@
+package student_vladislav_romanov.lesson_7.level_7;
+
+import java.util.Scanner;
+
+class UserEntity {
+
+    static int idBase = 0;
+    private int id;
+    private String name;
+    private String surname;
+    private String personalCode;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String text) {
+        id = ++idBase;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter user name: " );
+        name = scanner.next();
+
+        System.out.print("Please enter user surname: ");
+        surname = scanner.next();
+
+        System.out.print("Please enter user personal code: ");
+        personalCode = scanner.next();
+    }
+
+    public UserEntity(String name, String surname, String personalCode) {
+        id = ++idBase;
+        this.name = name;
+        this.surname = surname;
+        this.personalCode = personalCode;
+    }
+
+    public UserEntity(int id, String name, String surname, String personalCode) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.personalCode = personalCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPersonalCode() {
+        return personalCode;
+    }
+
+    public void setPersonalCode(String personalCode) {
+        this.personalCode = personalCode;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", personalCode='" + personalCode + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserEntity that = (UserEntity) o;
+        return id == that.id && name.equals(that.name) && surname.equals(that.surname) && personalCode.equals(that.personalCode);
+    }
+}
