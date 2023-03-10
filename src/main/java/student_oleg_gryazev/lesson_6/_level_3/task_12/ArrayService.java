6daca345f8ebd291259f140b182bcccceaad3347
Evroleg <evroleg@gmail.com>
2023-03-10 19:36:23 +0200

    Lesson 6 level 3 task 12

--- src/main/java/student_oleg_gryazev/lesson_6/_level_3/task_12/ArrayService.java
+++ src/main/java/student_oleg_gryazev/lesson_6/_level_3/task_12/ArrayService.java
@@ -2,14 +2,55 @@ package student_oleg_gryazev.lesson_6._level_3.task_12;
 
 public class ArrayService {
 
+
     boolean contains(int[] arr, int numberToSearch) {
-        for (int number : arr)
-            if (number == numberToSearch) {
+        // boolean searchNumber = false;
+        for (int element : arr)
+            if (element == numberToSearch) {
                 return true;
             }
-
         return false;
     }
 
+    int countNumberOccurrences(int[] arr) {
+        int numberOccurrences = 0;
+        for (int arrayNumber : arr) {
+            if (arrayNumber == 3) {
+                numberOccurrences += 1;
+            }
+        }
+        return numberOccurrences;
+    }
 
-}
+    boolean replaceFirstNumberInArray(int[] array, int numberToReplace, int newNumber) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == numberToReplace) {
+                array[i] = newNumber;
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    int replaceAllSimilarNumbers(int[] arr, int numberToReplace, int newNumber) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == numberToReplace) {
+                arr[i] = newNumber;
+
+            }
+        }
+        return newNumber;
+    }
+    void revert(int[] arr){
+        for (int i = 0; i < arr.length / 2; i++) {
+            int j = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = j;
+        }
+            }
+
+        }
+
+
+
--- src/main/java/student_oleg_gryazev/lesson_6/_level_3/task_12/TestArrayService.java
+++ src/main/java/student_oleg_gryazev/lesson_6/_level_3/task_12/TestArrayService.java
@@ -1,11 +1,18 @@
 package student_oleg_gryazev.lesson_6._level_3.task_12;
 
+import java.util.Arrays;
+
 public class TestArrayService {
 
     public static void main(String[] args) {
         TestArrayService arrayService = new TestArrayService();
         arrayService.ifArrayContainsGivenNumber();
         arrayService.ifArrayContainsNoGivenNumber();
+        arrayService.ifArrayContainsSimilarNumbers();
+        arrayService.ifArrayContainsNoSimilarNumbers();
+        arrayService.changingFirstElementInArray();
+        arrayService.changingAllSimilarElementInArray();
+        arrayService.massiveTurnOver();
     }
 
     ArrayService service = new ArrayService();
@@ -17,7 +24,7 @@ public class TestArrayService {
         if (contains) {
             System.out.println("Test has passed and matches numberToSearch = " + numberToSearch);
         } else {
-            System.out.println("Out of service");
+            System.out.println("Out of methode service");
         }
     }
 
@@ -28,8 +35,74 @@ public class TestArrayService {
         if (!contains) {
             System.out.println("Test has passed. Array does not have numberToSearch");
         } else {
-            System.out.println("Out of service");
+            System.out.println("Out of methode service");
+        }
+    }
+
+    void ifArrayContainsSimilarNumbers() {
+        int[] arr = {1, -1, 3, 45, 3};
+        int expectedResult = 2;
+
+        int realResult = service.countNumberOccurrences(arr);
+        if (expectedResult == realResult) {
+            System.out.println("Test is passed, expected result - " + expectedResult + " equals real result " + realResult);
+        } else {
+            System.out.println("Test false");
+        }
+    }
+
+    void ifArrayContainsNoSimilarNumbers() {
+        int[] arr = {1, -1, 33, 45, 3};
+        int expectedResult = 2;
+
+        int realResult = service.countNumberOccurrences(arr);
+        if (expectedResult != realResult) {
+            System.out.println("Test is passed, expected result no contains 2 expected similar numbers");
+        } else {
+            System.out.println("Test false");
+        }
+    }
+
+    void changingFirstElementInArray() {
+        ArrayService arrayService = new ArrayService();
+        int[] arr = {1, -1, 33, 45, 3};
+        boolean ok = arrayService.replaceFirstNumberInArray(arr, -1, 100);
+        if (ok && 100 == arr[1]) {
+            System.out.println("Second element of massive- " + Arrays.toString(arr) + " replaced with 100.");
+        } else {
+            System.out.println("Found and replace first occurrence in array" + " = FAIL!");
         }
 
     }
-}
+
+    void changingAllSimilarElementInArray() {
+
+        int[] arr = {3, -1, 3, 45, 3};
+        service.replaceAllSimilarNumbers(arr, 3, 200);
+        int[] changedMassive = {200, -1, 200, 45, 200};
+
+        if (Arrays.equals(arr, changedMassive)) {
+            System.out.println("All elements with number 3 of massive arr " + Arrays.toString(changedMassive) + " replaced with 200.");
+        } else {
+            System.out.println("hgjhg");
+        }
+    }
+
+    void massiveTurnOver() {
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] revertArr = {5, 4, 3, 2, 1};
+
+        service.revert(arr);
+
+            if (Arrays.equals(arr, revertArr)) {
+
+                    System.out.println("Test is successfull, initial massive is reverted. New arr = " + Arrays.toString(arr));
+                }
+                else System.out.println("Test is no successfull");
+            }
+
+        }
+
+
+
+
