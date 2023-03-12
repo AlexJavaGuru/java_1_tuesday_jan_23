821e8da294e8f06cb9b615fd31282f854a22dc6b
Vlad <wladislaw.romanov@gmail.com>
2023-03-12 13:53:10 +0300

    Lesson 8. Task 19

---
+++ src/main/java/student_vladislav_romanov/lesson_8/level_4/Triangle.java
@@ -0,0 +1,30 @@
+package student_vladislav_romanov.lesson_8.level_4;
+
+class Triangle extends Shape {
+
+    private double sideA;
+    private double sideB;
+    private double sideC;
+    private double mount;
+    private double height;
+
+    Triangle(String title, double sideA, double sideB, double sideC, double mount, double height) {
+        super(title);
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        this.mount = mount;
+        this.height = height;
+    }
+
+    @Override
+    double calculateArea() {
+        return 0.5 * mount * height;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return sideA + sideB + sideC;
+    }
+
+}
---
+++ src/main/java/student_vladislav_romanov/lesson_8/level_4/TriangleTest.java
@@ -0,0 +1,61 @@
+package student_vladislav_romanov.lesson_8.level_4;
+
+class TriangleTest {
+
+    private int succeedTests;
+    private int failedTests;
+
+    public static void main(String[] args) {
+        TriangleTest TriangleTest = new TriangleTest();
+        TriangleTest.calculateAreaTestSucceed();
+        TriangleTest.calculateAreaTestFailed();
+        TriangleTest.calculatePerimeterTestSucceed();
+        TriangleTest.calculatePerimeterTestFailed();
+        TriangleTest.printStat();
+    }
+
+    public void calculateAreaTestSucceed() {
+        Triangle triangle = new Triangle("Triangle", 3, 4, 5, 4, 3);
+        double expected = 6;
+        double current = triangle.calculateArea();
+        printResult(current == expected, "calculateAreaTestSucceed");
+    }
+
+    public void calculateAreaTestFailed() {
+        Triangle triangle = new Triangle("Triangle", 3, 4, 5, 4, 3);
+        double expected = 10;
+        double current = triangle.calculateArea();
+        printResult(current != expected, "calculateAreaTestFailed");
+    }
+
+    public void calculatePerimeterTestSucceed() {
+        Triangle triangle = new Triangle("Triangle", 3, 4, 5, 4, 3);
+        double expected = 12;
+        double current = triangle.calculatePerimeter();
+        printResult(current == expected, "calculatePerimeterTestSucceed");
+    }
+
+    public void calculatePerimeterTestFailed() {
+        Triangle triangle = new Triangle("Triangle", 3, 4, 5, 4, 3);
+        double expected = 13;
+        double current = triangle.calculatePerimeter();
+        printResult(current != expected, "calculatePerimeterTestFailed");
+    }
+
+    public void printResult(boolean result, String test) {
+        if (result) {
+            System.out.println(test + " test SUCCEED");
+            succeedTests++;
+        } else {
+            System.out.println(test + " test FAILED");
+            failedTests++;
+        }
+        System.out.println("----------------------------------------------------------");
+    }
+
+    public void printStat() {
+        System.out.println(succeedTests + " tests SUCCEED.");
+        System.out.println(failedTests + " tests FAILED.");
+    }
+
+}
