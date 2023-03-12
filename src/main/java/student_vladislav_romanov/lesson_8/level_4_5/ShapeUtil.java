da3b0582d7879043e2e94c1fb06c0191201c9e22
Vlad <wladislaw.romanov@gmail.com>
2023-03-12 14:03:14 +0300

    Lesson 8. Task 23

--- src/main/java/student_vladislav_romanov/lesson_8/level_4_5/ShapeUtil.java
+++ src/main/java/student_vladislav_romanov/lesson_8/level_4_5/ShapeUtil.java
@@ -7,14 +7,18 @@ class ShapeUtil {
     Random random = new Random();
 
     Circle createRandomCircle() {
-        return new Circle("Circle", random.nextDouble(100));
+        return new Circle("Circle", random.nextDouble(10));
     }
 
     Square createRandomSquare() {
-        return new Square("Square", random.nextDouble(100));
+        return new Square("Square", random.nextDouble(10));
     }
 
     Rectangle createRandomRectangle() {
-        return new Rectangle("Rectangle", random.nextDouble(100), random.nextDouble(100));
+        return new Rectangle("Rectangle", random.nextDouble(10), random.nextDouble(10));
+    }
+
+    Triangle createRandomTriangle() {
+        return new Triangle("Triangle", random.nextDouble(10), random.nextDouble(10));
     }
 }
