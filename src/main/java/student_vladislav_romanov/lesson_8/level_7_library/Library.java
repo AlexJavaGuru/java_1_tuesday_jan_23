51af5205d0520174ff834892f4eca2c81f84f02d
Vlad <wladislaw.romanov@gmail.com>
2023-03-13 19:48:28 +0300

    Lesson 8. Task 31. Cleanup

--- src/main/java/student_vladislav_romanov/lesson_8/level_7_library/Library.java
+++ src/main/java/student_vladislav_romanov/lesson_8/level_7_library/Library.java
@@ -8,37 +8,4 @@ class Library {
     private Reader[] readers;
     private Book[] takenBooks;
 
-    public Book[] getBooks() {
-        return books;
-    }
-
-    public void setBooks(Book[] books) {
-        this.books = books;
-    }
-
-    public Reader[] getReaders() {
-        return readers;
-    }
-
-    public void setReaders(Reader[] readers) {
-        this.readers = readers;
-    }
-
-    public Book[] getTakenBooks() {
-        return takenBooks;
-    }
-
-    public void setTakenBooks(Book[] takenBooks) {
-        this.takenBooks = takenBooks;
-    }
-
-    @Override
-    public String toString() {
-        return "Library{" +
-                "books=" + Arrays.toString(books) +
-                ", readers=" + Arrays.toString(readers) +
-                ", takenBooks=" + Arrays.toString(takenBooks) +
-                '}';
-    }
-
 }
--- src/main/java/student_vladislav_romanov/lesson_8/level_7_library/Reader.java
+++ src/main/java/student_vladislav_romanov/lesson_8/level_7_library/Reader.java
@@ -1,8 +1,5 @@
 package student_vladislav_romanov.lesson_8.level_7_library;
 
-import java.util.Arrays;
-import java.util.Objects;
-
 class Reader {
 
     private long id;
@@ -12,70 +9,4 @@ class Reader {
     private Book[] gotBooks;
     private double ticketsAmount;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Book[] getGotBooks() {
-        return gotBooks;
-    }
-
-    public void setGotBooks(Book[] gotBooks) {
-        this.gotBooks = gotBooks;
-    }
-
-    public double getTicketsAmount() {
-        return ticketsAmount;
-    }
-
-    public void setTicketsAmount(double ticketsAmount) {
-        this.ticketsAmount = ticketsAmount;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Reader reader = (Reader) o;
-        return id == reader.id && Objects.equals(name, reader.name) && Objects.equals(surname, reader.surname) && Objects.equals(email, reader.email) && Arrays.equals(gotBooks, reader.gotBooks);
-    }
-
-    @Override
-    public String toString() {
-        return "Reader{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", email='" + email + '\'' +
-                ", gotBooks=" + Arrays.toString(gotBooks) +
-                '}';
-    }
 }
