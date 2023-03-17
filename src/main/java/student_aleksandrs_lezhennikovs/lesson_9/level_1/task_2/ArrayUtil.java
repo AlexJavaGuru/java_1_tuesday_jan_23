5c4b7d42e4b3db098d25dd2e40451f92a07dba7b
AlexandrLezh <sasha_l@inbox.lv>
2023-03-17 20:12:01 +0200

    lesson_9/level_1

---
+++ src/main/java/student_aleksandrs_lezhennikovs/lesson_9/level_1/task_2/ArrayUtil.java
@@ -0,0 +1,8 @@
+package student_aleksandrs_lezhennikovs.lesson_9.level_1.task_2;
+
+interface ArrayUtil {
+    void fillArrayWithRandomNumbers(int[] array);
+    void printArrayToConsole(int[] array);
+    int findMaxNumber(int[] array);
+    int findMinNumber(int[] array);
+}
---
+++ src/main/java/student_aleksandrs_lezhennikovs/lesson_9/level_1/task_3/MyList.java
@@ -0,0 +1,11 @@
+package student_aleksandrs_lezhennikovs.lesson_9.level_1.task_3;
+
+interface MyList {
+
+    void add(Object o);
+    void delete(Object o);
+    void sort(Object o);
+    void printAll(Object o);
+    Object search(long id);
+
+}
