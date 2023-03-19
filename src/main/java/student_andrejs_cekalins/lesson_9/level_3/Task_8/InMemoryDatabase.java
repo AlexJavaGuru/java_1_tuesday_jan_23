5ebf5ced49c914b49c9210d38c16d6f26d5df1f4
AndrejsCekalins <batarejka2006@gmail.com>
2023-03-19 17:53:47 +0200

    AC_lesson_9

--- src/main/java/student_andrejs_cekalins/lesson_9/level_3/Task_8/InMemoryDatabase.java
+++ src/main/java/student_andrejs_cekalins/lesson_9/level_3/Task_8/InMemoryDatabase.java
@@ -2,8 +2,10 @@ package student_andrejs_cekalins.lesson_9.level_3.Task_8;
 
 class InMemoryDatabase implements ProductDatabase{
 
+
     @Override
     public void save(Product product) {
+        int index =products.len
 
 
     }
