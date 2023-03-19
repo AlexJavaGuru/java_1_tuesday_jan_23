73ebab20b52f330d16d406b7928f60bbb28d2ace
AndrejsCekalins <batarejka2006@gmail.com>
2023-03-19 19:48:03 +0200

    AC_lesson_9

--- src/main/java/student_andrejs_cekalins/lesson_9/level_3/Task_8/InMemoryDatabase.java
+++ src/main/java/student_andrejs_cekalins/lesson_9/level_3/Task_8/InMemoryDatabase.java
@@ -1,17 +1,35 @@
 package student_andrejs_cekalins.lesson_9.level_3.Task_8;
 
-class InMemoryDatabase implements ProductDatabase{
+import java.util.Objects;
+
+class InMemoryDatabase implements ProductDatabase {
+    private Product[] products;
 
 
     @Override
     public void save(Product product) {
-        int index =products.len
-
-
+        int indexOfEmptyElement = getIndexOfEmptyElement(products);
+        if (indexOfEmptyElement != -1) {
+            products[indexOfEmptyElement] = product;
+        }
     }
 
     @Override
     public Product findByTitle(String productTitle) {
+        for (Product product : products) {
+            if (Objects.equals(productTitle, product.getTitle())) {
+                return product;
+            }
+        }
         return null;
+    }
+
+    private int getIndexOfEmptyElement(Product[] products) {
+        for (int i = 0; i < products.length; i++) {
+            if(products[i]==null) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
---
+++ src/main/java/student_andrejs_cekalins/lesson_9/level_3/Task_8/ProductDatabaseTest.java
@@ -0,0 +1,23 @@
+package student_andrejs_cekalins.lesson_9.level_3.Task_8;
+
+class ProductDatabaseTest {
+    public static void main(String[] args) {
+        ProductDatabaseTest test = new ProductDatabaseTest();
+        test.findByTitle();
+    }
+
+    public void findByTitle() {
+        ProductDatabase productDatabase = new InMemoryDatabase();
+        Product[] products = {
+                new Product("lemon"),
+                new Product("melon"),
+                new Product("apple"),
+                new Product("pear")
+        };
+        Product expectedResult = products[2];
+        Product realResult = productDatabase.findByTitle("apple");
+        if (expectedResult.equals(realResult)) {
+            System.out.println("Find by title test Passed");
+        }
+    }
+}
