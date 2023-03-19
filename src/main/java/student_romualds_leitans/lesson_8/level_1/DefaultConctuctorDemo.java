0acd843b835e1ffe39bf3d7c959e2547f0bfafec
Romualds Leitans <Romualdlei@gmail.com>
2023-03-19 19:53:26 +0000

    RL Lesson 8

---
+++ src/main/java/student_romualds_leitans/lesson_8/level_1/DefaultConctuctorDemo.java
@@ -0,0 +1,9 @@
+package student_romualds_leitans.lesson_8.level_1;
+
+public class DefaultConctuctorDemo {
+
+    public static void main(String[] args) {
+
+        DefaultConstructor defaultConstructor = new DefaultConstructor();
+    }
+}
