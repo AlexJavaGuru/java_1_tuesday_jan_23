9b0d9fb7085288acf6669c88951a7a0539c889a6
Vlad <wladislaw.romanov@gmail.com>
2023-03-20 07:26:38 +0300

    Lesson 9. Task 5

---
+++ src/main/java/student_vladislav_romanov/lesson_9/level_2/DayOfTheWeekDetectorDemo.java
@@ -0,0 +1,11 @@
+package student_vladislav_romanov.lesson_9.level_2;
+
+class DayOfTheWeekDetectorDemo {
+
+    DayOfTheWeekDetectorDemo(DayOfTheWeekDetector detectionWay) {
+    }
+
+    void run() {
+
+    }
+}
