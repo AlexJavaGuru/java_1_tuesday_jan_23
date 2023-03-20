74ab854c43514ce6f39f7e12dd58b05e534916b0
Vlad <wladislaw.romanov@gmail.com>
2023-03-20 21:23:22 +0300

    Lesson 9. Task 6

---
+++ src/main/java/student_vladislav_romanov/lesson_9/level_3/Task_6.java
@@ -0,0 +1,12 @@
+package student_vladislav_romanov.lesson_9.level_3;
+
+/*
+* Функциональным считается интерфейс с одним не реализованным (абстрактным) методом.
+*/
+
+@FunctionalInterface
+public interface Task_6 {
+
+    void sayHello();
+
+}
