16eebe4f67a5297309bef260d4a7441373bf19bc
Vlad <wladislaw.romanov@gmail.com>
2023-03-21 18:23:14 +0300

    Lesson 9. Task 11, 12

---
+++ src/main/java/student_vladislav_romanov/lesson_9/level_4/CelsiusConverter.java
@@ -0,0 +1,10 @@
+package student_vladislav_romanov.lesson_9.level_4;
+
+class CelsiusConverter implements TemperatureConverter {
+
+    @Override
+    public double convert(double celsiusTemperature) {
+        return celsiusTemperature;
+    }
+
+}
---
+++ src/main/java/student_vladislav_romanov/lesson_9/level_4/TemperatureConverter.java
@@ -0,0 +1,7 @@
+package student_vladislav_romanov.lesson_9.level_4;
+
+interface TemperatureConverter {
+
+    double convert(double celsiusTemperature);
+
+}
