fc00e9a3abbf6c922b47b0d267b53a67809e3967
Оксана Tarasova <oksana_sineok@mail.ru>
2023-03-23 22:44:24 +0200

    commit OksTar_lesson 8 corrected

--- src/main/java/student_oksana_tarasova/lesson_8/level_1/Person.java
+++ src/main/java/student_oksana_tarasova/lesson_8/level_1/Person.java
@@ -1,29 +1,5 @@
 package student_oksana_tarasova.lesson_8.level_1;
 
 class Person extends Object {
-    String name;
-    String surname;
-    String address;
-    String nationality;
-    String profession;
 
-    public Person(int age, double height, boolean hunger, String name, String surname, String address, String nationality, String profession) {
-        super(age, height, hunger);
-        this.name = name;
-        this.surname = surname;
-        this.address = address;
-        this.nationality = nationality;
-        this.profession = profession;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", address='" + address + '\'' +
-                ", nationality='" + nationality + '\'' +
-                ", profession='" + profession + '\'' +
-                "} " + super.toString();
-    }
 }
