221c146259439af5ad80929f29cd22711a3a1d29
Оксана Tarasova <oksana_sineok@mail.ru>
2023-03-23 21:00:10 +0200

    commit OksTar_lesson 9 level 7

---
+++ src/main/java/student_oksana_tarasova/Lesson_9/level_7/Circle.java
@@ -0,0 +1,19 @@
+package student_oksana_tarasova.Lesson_9.level_7;
+
+public class Circle implements Shape {
+   private double radius;
+
+
+
+
+
+    @Override
+    public double calculatePerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 0;
+    }
+}
---
+++ src/main/java/student_oksana_tarasova/Lesson_9/level_7/Shape.java
@@ -0,0 +1,8 @@
+package student_oksana_tarasova.Lesson_9.level_7;
+
+public interface Shape {
+
+    double calculatePerimeter();
+
+    double calculateArea();
+}
