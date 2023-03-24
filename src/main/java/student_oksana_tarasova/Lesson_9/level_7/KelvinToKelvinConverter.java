31514cc7d999f97ff9f97ec19851a679aced6f36
Оксана Tarasova <oksana_sineok@mail.ru>
2023-03-24 21:29:12 +0200

    commit OksTar_lesson 9 level_7

---
+++ src/main/java/student_oksana_tarasova/Lesson_9/level_7/KelvinToKelvinConverter.java
@@ -0,0 +1,9 @@
+package student_oksana_tarasova.Lesson_9.level_7;
+
+class KelvinToKelvinConverter implements ConverterTemperature {
+
+    @Override
+    public double convert(double kelvinTemperature) {
+        return kelvinTemperature;
+    }
+}
--- src/main/java/student_oksana_tarasova/Lesson_9/level_7/Shape.java
+++
@@ -1,8 +0,0 @@
-package student_oksana_tarasova.Lesson_9.level_7;
-
-public interface Shape {
-
-    double calculatePerimeter();
-
-    double calculateArea();
-}
