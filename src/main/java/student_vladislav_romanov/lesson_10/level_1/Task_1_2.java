2da288c9c2dd3546aba45a4cb0c1e5fa8d01aec6
Vlad <wladislaw.romanov@gmail.com>
2023-03-26 15:09:06 +0300

    Lesson 10. Task 1, 2

---
+++ src/main/java/student_vladislav_romanov/lesson_10/level_1/Task_1_2.java
@@ -0,0 +1,24 @@
+package student_vladislav_romanov.lesson_10.level_1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class Task_1_2 {
+
+    public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<>();
+        List<Integer> linkedList = new LinkedList<>();
+
+        arrayList.add("Hello");
+        arrayList.add("Java");
+        arrayList.add("World");
+        System.out.println(arrayList);
+
+        linkedList.add(3);
+        linkedList.add(7);
+        linkedList.add(11);
+        System.out.println(linkedList);
+
+    }
+}
