c8a0fd05ecf2ec9a96a49c2decc5d346d344d6ba
Vlad <wladislaw.romanov@gmail.com>
2023-03-26 12:55:55 +0300

    Lesson 9. Task 27

--- src/main/java/student_vladislav_romanov/lesson_9/level_7/MyList.java
+++ src/main/java/student_vladislav_romanov/lesson_9/level_7/MyList.java
@@ -3,10 +3,10 @@ package student_vladislav_romanov.lesson_9.level_7;
 interface MyList {
 
     // Add new element to List
-    void addElement(String element);
+    void addElement(String text);
 
     // Remove element from List by content int
-    void removeElement(String element);
+    void removeElement(String text);
 
     // Sorting List ASC, return sorted List
     void sortAsc();
@@ -15,6 +15,6 @@ interface MyList {
     void sortDesc();
 
     // Find element, return element if existed, else null
-    String find(String element);
+    String find(String text);
 
 }
