096e3dfc2c62665f097179f4ca238a620c90a9e8
Vlad <wladislaw.romanov@gmail.com>
2023-03-26 20:08:23 +0300

    Lesson 10. Task 16

---
+++ src/main/java/student_vladislav_romanov/lesson_10/level_2/AuthorSearchCriteria.java
@@ -0,0 +1,15 @@
+package student_vladislav_romanov.lesson_10.level_2;
+
+class AuthorSearchCriteria implements SearchCriteria {
+
+    private String authorToSearch;
+
+    public AuthorSearchCriteria(String authorToSearch) {
+        this.authorToSearch = authorToSearch;
+    }
+
+    public boolean match(Book book) {
+        return book.getAuthor().toLowerCase().contains(authorToSearch.toLowerCase());
+    }
+
+}
