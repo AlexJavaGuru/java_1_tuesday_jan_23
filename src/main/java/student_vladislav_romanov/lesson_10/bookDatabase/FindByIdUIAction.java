057ffa7a709a3bb0d080c374bbf88135da12a522
Vlad <wladislaw.romanov@gmail.com>
2023-03-27 20:34:57 +0300

    Lesson 10. Task 32

---
+++ src/main/java/student_vladislav_romanov/lesson_10/bookDatabase/FindByIdUIAction.java
@@ -0,0 +1,22 @@
+package student_vladislav_romanov.lesson_10.bookDatabase;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+public class FindByIdUIAction implements UIAction {
+
+    private BookDatabase bookDatabase;
+
+    public FindByIdUIAction(BookDatabase bookDatabase) {
+        this.bookDatabase = bookDatabase;
+    }
+
+    @Override
+    public void execute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Пожалуйста введите id книги: ");
+        long id = scanner.nextLong();
+        Optional<Book> bookOpt = bookDatabase.findById(id);
+        System.out.println(bookOpt.isEmpty() ? "Книги с таким идентификатором не существует" : bookOpt);
+    }
+}
