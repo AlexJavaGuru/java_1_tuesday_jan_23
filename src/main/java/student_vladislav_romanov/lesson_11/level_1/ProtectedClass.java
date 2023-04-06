f6c8dda20b30b89297104f37d09a30b2cd937110
Vlad <wladislaw.romanov@gmail.com>
2023-04-06 19:13:28 +0300

    Lesson 11. Task 8

---
+++ src/main/java/student_vladislav_romanov/lesson_11/level_1/ProtectedClass.java
@@ -0,0 +1,10 @@
+package student_vladislav_romanov.lesson_11.level_1;
+
+// Защищённым может быть только внутренний класс, иначе к нему не будет доступа.
+
+public class ProtectedClass {
+
+    private String title;
+    private String author;
+
+}
