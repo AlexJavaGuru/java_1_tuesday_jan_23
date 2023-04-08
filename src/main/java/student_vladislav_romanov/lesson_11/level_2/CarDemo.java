128bb2f0a7c392e08dbbc2330c3a0c02ebdeb6bc
Vlad <wladislaw.romanov@gmail.com>
2023-04-08 22:23:48 +0300

    Lesson 11. Task 11

---
+++ src/main/java/student_vladislav_romanov/lesson_11/level_2/CarDemo.java
@@ -0,0 +1,9 @@
+package student_vladislav_romanov.lesson_11.level_2;
+
+public class CarDemo {
+
+    public static void main(String[] args) {
+        Car car = new Car("BMW", "320d", "mystic blue");
+        car.drive();
+    }
+}
