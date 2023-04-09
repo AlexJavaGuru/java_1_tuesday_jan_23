9c7fee8f2d96ac604bdd1304e142efa6182f2817
IchiuRain <ichiu.rain011@gmail.com>
2023-04-09 09:38:16 +0300

    ABok_lesson9-1

---
+++ src/main/java/student_anastasiia_bokareva/lesson9/level1/JavaStartLoginServiceImpl.java
@@ -0,0 +1,13 @@
+package student_anastasiia_bokareva.lesson9.level1;
+
+public class JavaStartLoginServiceImpl implements JavaStarLoginService {
+
+    @Override
+    public boolean canLogin(String login, String password) {
+        return (login != null)
+                && login.equals("javastar")
+                && (password != null)
+                && password.equals("pass");
+    }
+}
+
---
+++ src/main/java/student_anastasiia_bokareva/lesson9/level1/MyList.java
@@ -0,0 +1,8 @@
+package student_anastasiia_bokareva.lesson9.level1;
+
+public interface MyList {
+    void delete (Object 0 ); // удаление бьекта
+    void add (Object 0); // добавление нового обьекта
+    void change (Object 0); // изменение обьекта
+
+}
