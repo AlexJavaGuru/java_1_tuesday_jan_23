85ba3b1851ff9f1db956c91e1a89b9ed65f5a90d
Vlad <wladislaw.romanov@gmail.com>
2023-04-09 17:12:11 +0300

    Lesson 11. Task 27

--- src/main/java/student_vladislav_romanov/lesson_11/level_4_5_6/FraudRule4.java
+++ src/main/java/student_vladislav_romanov/lesson_11/level_4_5_6/FraudRule4.java
@@ -1,11 +1,11 @@
 package student_vladislav_romanov.lesson_11.level_4_5_6;
 
-public class FraudRule4 implements FraudRule {
+public class FraudRule4 extends FraudRule {
 
     private String ruleName;
 
     public FraudRule4(String ruleName) {
-        this.ruleName = ruleName;
+        super(ruleName);
     }
 
     public String getRuleName() {
