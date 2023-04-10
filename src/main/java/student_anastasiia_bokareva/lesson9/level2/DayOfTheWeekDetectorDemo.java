344123283c08fb42b52d06a61dbfe2ae5621020d
IchiuRain <ichiu.rain011@gmail.com>
2023-04-10 10:35:17 +0300

    ABok_lesson9-level2

--- src/main/java/student_anastasiia_bokareva/lesson9/level2/DayOfTheWeekDetectorDemo.java
+++ src/main/java/student_anastasiia_bokareva/lesson9/level2/DayOfTheWeekDetectorDemo.java
@@ -1,4 +1,11 @@
 package student_anastasiia_bokareva.lesson9.level2;
 
+import java.util.Scanner;
+
 public class DayOfTheWeekDetectorDemo {
+    private DayOfTheWeekDetector dayDetector;
+    DayOfTheWeekDetectorDemo( DayOfTheWeekDetector dayDetector){
+        this.dayDetector = dayDetector;
+    }
+
 }
