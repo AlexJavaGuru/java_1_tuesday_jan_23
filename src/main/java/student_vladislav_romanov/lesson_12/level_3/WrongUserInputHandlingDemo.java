47bbac54520d232cff6f218e83118c8db1a47390
Vlad <wladislaw.romanov@gmail.com>
2023-04-16 17:36:15 +0300

    Lesson 12. Task 19

---
+++ src/main/java/student_vladislav_romanov/lesson_12/level_3/WrongUserInputHandlingDemo.java
@@ -0,0 +1,29 @@
+package student_vladislav_romanov.lesson_12.level_3;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class WrongUserInputHandlingDemo {
+
+    public static void main(String[] args) {
+        System.out.print("Пожалуйста введите число: ");
+        while(true) {
+            if (isInputValid()) {
+                break;
+            }
+        }
+        System.out.println("Спасибо!");
+    }
+
+    public static boolean isInputValid() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.print("Эээ, русским же языком попросили ввести ЧИСЛО. Введите ещё раз: ");
+            return false;
+        }
+        return true;
+    }
+
+}
