f3899a0250776714ea11037a8b43c535278e7ffa
Vlad <wladislaw.romanov@gmail.com>
2023-04-17 21:56:51 +0300

    Lesson 13. Task 7

--- src/main/java/student_vladislav_romanov/lesson_13/level_1/TaxCalculatorImpl.java
+++ src/main/java/student_vladislav_romanov/lesson_13/level_1/TaxCalculatorImpl.java
@@ -6,7 +6,15 @@ class TaxCalculatorImpl implements TaxCalculator {
 
     @Override
     public BigDecimal calculateTax(BigDecimal income) {
-        return income.multiply(new BigDecimal("0.25"));
+        if (income.compareTo(new BigDecimal("20000.00")) <= 0) {
+            return income.multiply(new BigDecimal("0.25"));
+        } else {
+            BigDecimal firstStageIncome = new BigDecimal(20000);
+            BigDecimal secondStageIncome = income.subtract(firstStageIncome);
+            BigDecimal firstStageIncomeTax = firstStageIncome.multiply(new BigDecimal("0.25"));
+            BigDecimal secondStageIncomeTax = secondStageIncome.multiply(new BigDecimal("0.40"));
+            return firstStageIncomeTax.add(secondStageIncomeTax);
+        }
     }
 
 }
