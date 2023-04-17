0435431b088a688fbe4010da9d5658664e2bb8e3
Vlad <wladislaw.romanov@gmail.com>
2023-04-17 21:22:58 +0300

    Lesson 13. Task 2

---
+++ src/main/java/student_vladislav_romanov/lesson_13/level_1/TaxCalculatorImpl.java
@@ -0,0 +1,12 @@
+package student_vladislav_romanov.lesson_13.level_1;
+
+import java.math.BigDecimal;
+
+class TaxCalculatorImpl implements TaxCalculator {
+
+    @Override
+    public BigDecimal calculateTax(BigDecimal income) {
+        return null;
+    }
+
+}
