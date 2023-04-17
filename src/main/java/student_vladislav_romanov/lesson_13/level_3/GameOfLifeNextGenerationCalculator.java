7363582ffc2969381773f9d326fb76145dcafc2c
Vlad <wladislaw.romanov@gmail.com>
2023-04-17 22:07:38 +0300

    Lesson 13. Task 10

---
+++ src/main/java/student_vladislav_romanov/lesson_13/level_3/GameOfLifeNextGenerationCalculator.java
@@ -0,0 +1,9 @@
+package student_vladislav_romanov.lesson_13.level_3;
+
+class GameOfLifeNextGenerationCalculator {
+
+    public boolean[][] calculate(boolean[][] currentGeneration) {
+        return null;
+    }
+
+}
