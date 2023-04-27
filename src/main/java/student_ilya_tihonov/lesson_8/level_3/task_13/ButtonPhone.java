476573756468cb641ab7b275b998daa180e70c4d
Ilya <ilya.tihonov@inbox.lv>
2023-04-27 23:08:50 +0300

    IT : Lesson 8: corrected

--- src/main/java/student_ilya_tihonov/lesson_8/level_3/task_13/ButtonPhone.java
+++ src/main/java/student_ilya_tihonov/lesson_8/level_3/task_13/ButtonPhone.java
@@ -1,20 +1,24 @@
 package student_ilya_tihonov.lesson_8.level_3.task_13;
 
-abstract class Nokia3310 {
+class ButtonPhone {
 
     String brand;
     int releaseYear ;
     String model;
 
-    public Nokia3310(String brand, int releaseYear, String model) {
+    public ButtonPhone(String brand, int releaseYear, String model) {
         this.brand = brand;
         this.releaseYear = releaseYear;
         this.model = model;
     }
 
-    public abstract void shootVideo();
+    public void shootVideo() {
 
-    public abstract void sendAMessage();
+    }
+
+    public void sendAMessage(){
+
+    }
 
 
 }
