0da98276fb2555684bd1a9e922dae2ecc461ec7e
Vlad <wladislaw.romanov@gmail.com>
2023-05-01 21:44:08 +0300

    Lesson 14. Task 22

--- src/main/java/student_vladislav_romanov/lesson_14/level_3_4_5_6/TransactionAnalysisService.java
+++ src/main/java/student_vladislav_romanov/lesson_14/level_3_4_5_6/TransactionAnalysisService.java
@@ -1,5 +1,6 @@
 package student_vladislav_romanov.lesson_14.level_3_4_5_6;
 
+import java.util.Comparator;
 import java.util.List;
 
 class TransactionAnalysisService {
@@ -8,6 +9,20 @@ class TransactionAnalysisService {
         return transactions.stream()
                 .filter(transaction -> transaction.year() == year)
                 .toList();
+    }
+
+    List<Transaction> sortingTransactions(List<Transaction> transactions, String method) {
+        if ("asc".equals(method)) {
+            return transactions.stream()
+                    .sorted(Comparator.comparingInt(Transaction::value))
+                    .toList();
+        } else if ("desc".equals(method)) {
+            return transactions.stream()
+                    .sorted(Comparator.comparingInt(Transaction::value).reversed())
+                    .toList();
+        } else {
+            return transactions;
+        }
     }
 
 }
--- src/test/java/student_vladislav_romanov/lesson_14/level_3_4_5_6/TransactionAnalysisServiceTest.java
+++ src/test/java/student_vladislav_romanov/lesson_14/level_3_4_5_6/TransactionAnalysisServiceTest.java
@@ -12,11 +12,13 @@ class TransactionAnalysisServiceTest {
 
     TransactionTestData transactionTestData;
     TransactionAnalysisService transactionAnalysisService;
+    List<Transaction> transactions;
 
     @BeforeEach
     void setUp() {
         transactionTestData = new TransactionTestData();
         transactionAnalysisService = new TransactionAnalysisService();
+        transactions = transactionTestData.getTransactions();
     }
 
     @Test
@@ -27,7 +29,45 @@ class TransactionAnalysisServiceTest {
                 new Transaction(brian, 2011, 300),
                 new Transaction(raoul, 2011, 400)
         );
-        List<Transaction> actual = transactionAnalysisService.getTransactionsByYear(transactionTestData.getTransactions(), 2011);
+        List<Transaction> actual = transactionAnalysisService.getTransactionsByYear(transactions, 2011);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void sortingTransactionsAsc() {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario","Milan");
+        Trader alan = new Trader("Alan","Cambridge");
+        Trader brian = new Trader("Brian","Cambridge");
+
+        List<Transaction> expected = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(mario, 2012, 700),
+                new Transaction(mario, 2012, 710),
+                new Transaction(alan, 2012, 950),
+                new Transaction(raoul, 2012, 1000)
+        );
+        List<Transaction> actual = transactionAnalysisService.sortingTransactions(transactions, "asc");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void sortingTransactionsDesc() {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario","Milan");
+        Trader alan = new Trader("Alan","Cambridge");
+        Trader brian = new Trader("Brian","Cambridge");
+
+        List<Transaction> expected = Arrays.asList(
+                new Transaction(raoul, 2012, 1000),
+                new Transaction(alan, 2012, 950),
+                new Transaction(mario, 2012, 710),
+                new Transaction(mario, 2012, 700),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(brian, 2011, 300)
+        );
+        List<Transaction> actual = transactionAnalysisService.sortingTransactions(transactions, "desc");
         assertEquals(expected, actual);
     }
 
