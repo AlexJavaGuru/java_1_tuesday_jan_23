aa509d96686a167ba4a61a7936534f8342fa7c4f
Vlad <wladislaw.romanov@gmail.com>
2023-05-01 20:45:46 +0300

    Lesson 14. Task 15

--- src/main/java/student_vladislav_romanov/lesson_14/level_1/FruitPredicate.java
+++ src/main/java/student_vladislav_romanov/lesson_14/level_1/FruitPredicate.java
@@ -2,6 +2,7 @@ package student_vladislav_romanov.lesson_14.level_1;
 
 import java.util.function.Predicate;
 
+@FunctionalInterface
 public interface FruitPredicate extends Predicate<Apple> {
 
     @Override
