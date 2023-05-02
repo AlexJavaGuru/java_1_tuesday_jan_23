636fdfbee35ba057618832a204f059d2b94bbb62
Vlad <wladislaw.romanov@gmail.com>
2023-05-02 15:08:19 +0300

    Lesson 15. Task 7

--- src/main/java/student_vladislav_romanov/lesson_15/level_2/QuadraticEq.java
+++ src/main/java/student_vladislav_romanov/lesson_15/level_2/QuadraticEq.java
@@ -2,23 +2,23 @@ package student_vladislav_romanov.lesson_15.level_2;
 
 class QuadraticEq {
 
-    public void calc(double a, double b, double c) {
+    public String calc(double a, double b, double c) {
         double discriminant = b * b - 4 * a * c;
         if (discriminant > 0) {
-            System.out.println(getRoot(a, b, discriminant));
+            return getRoot(a, b, discriminant);
         }
         else if (discriminant == 0) {
-            System.out.println(getRoot(a, b));
+            return getRoot(a, b);
         }
         else {
-            System.out.println("Equation has no roots");
+            return "Equation has no roots";
         }
     }
 
     String getRoot(double a, double b) {
         double x;
         x = -b / (2 * a);
-        return ("x = " + x);
+        return "x = " + x;
     }
 
     String getRoot(double a, double b, double discriminant) {
@@ -26,7 +26,7 @@ class QuadraticEq {
         double x2;
         x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
         x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
-        return ("x1 = " + x1 + ", x2 = " + x2);
+        return "x1 = " + x1 + ", x2 = " + x2;
     }
 
 }
